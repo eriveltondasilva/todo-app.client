bc0c373f4a7901f788b80ce38b60ffae74d9b9c6
Erivelton Silva <eriveltondasilva13@gmail.com>
2024-01-25 06:40:30 -0300

    build: configura tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'mobile-light': 'url("../assets/images/bg-mobile-light.jpg")',
+        'mobile-dark': 'url("../assets/images/bg-mobile-dark.jpg")',
+        'desktop-dark': 'url("../assets/images/bg-desktop-dark.jpg")',
+        'desktop-light': 'url("../assets/images/bg-desktop-light.jpg")',
+      },
+      colors: {
+        primary: 'hsl(220, 98%, 61%)',
+        surface: {
+          light: 'hsl(0, 0%, 98%)',
+          dark: 'hsl(235, 24%, 19%)',
+        },
+        background: {
+          light: 'hsl(236, 33%, 92%)',
+          dark: 'hsl(235, 21%, 11%)',
+        },
+        gradient: {
+          left: 'hsl(192, 100%, 67%)',
+          right: 'hsl(280, 87%, 65%)',
+        },
       },
     },
   },
