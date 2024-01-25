0688588985131f76e37766244beae8d95f143f0d
Erivelton Silva <eriveltondasilva13@gmail.com>
2024-01-25 09:10:51 -0300

    api

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 // ------------------------------------
 const axiosApi = axios.create({
-  baseURL: API_URL,
+  baseURL: `${API_URL}/api/`,
 })
 
 export default axiosApi
