e62f62582abb3c3dad0e13b4ffd6e5e573f10492
Erivelton Silva <eriveltondasilva13@gmail.com>
2024-06-29 03:14:16 -0300

    adiciona fetch

--- src/services/taskApi.ts
+++ src/services/taskApi.ts
@@ -1,6 +1,5 @@
 import axios from './axios'
 
-// ============================================================================
 async function api(method: string, url: string, data?: any): Promise<any> {
   try {
     return await axios({ method, url, data })
@@ -10,32 +9,27 @@ async function api(method: string, url: string, data?: any): Promise<any> {
   }
 }
 
-// -----------------------------------------------------------------------------
 export async function getAllTasks() {
   const url = '/api/tasks'
   return api('get', url)
 }
 
-// -----------------------------------------------------------------------------
 export async function createTask(formData: any) {
   const data = Object.fromEntries(formData)
   const url = '/api/tasks'
   return api('post', url, data)
 }
 
-// ----------------------------------------------------------------------------
 export async function updateTask(id: number, data: any) {
   const url = `/api/tasks/${id}`
   return api('put', url, data)
 }
 
-// ----------------------------------------------------------------------------
 export async function deleteTask(id: number) {
   const url = `/api/tasks/${id}`
   return api('delete', url)
 }
 
-// ----------------------------------------------------------------------------
 export async function deleteTasks(ids: number[]) {
   const url = '/api/tasks'
   const data = { ids }
